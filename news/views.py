94c7ac639dbc1ad7c4746177ba340229b0dd69b9
anastasia <petrichuk.nastya@yandex.ru>
2021-06-13 16:03:33 +0300

    оптимизированы html файлы

--- news/views.py
+++ news/views.py
@@ -5,4 +5,4 @@ from .models import News
 
 def index(request):
     news = News.objects.all()[:8]       # получение последних 8 новостей
-    return render(request, 'news/blog.html', {'news': news})
+    return render(request, 'news/index.html', {'news': news, 'active_link': 'blog'})
